94617359545d53c4b70c6ce80d5a1b60c921b66c
Tien <jellybui90@gmail.com>
2018-06-15 16:50:49 -0700

    Add files for cart

--- Tutorial/Controllers/CartController.cs
+++ Tutorial/Controllers/CartController.cs
@@ -10,16 +10,23 @@ namespace Tutorial.Controllers
 {
     public class CartController : Controller
     {
+        public ActionResult themgiohang(int maSach = 0, int soLuongMua = 0)
+        {
             Cart cart = (Cart)Session["cart"];
             Sach sach = SachDAO.getSachCanLay(maSach);
             if (cart.getCartItems().ContainsKey(maSach))
             {
-                cart.plusTocart(maSach,new Item(sach,cart.getCartItems()[maSach].getSoLuongBan()),soLuongMua);
+                cart.plusTocart(maSach, new Item(sach, cart.getCartItems()[maSach].getSoLuongBan()), soLuongMua);
             }
             else
             {
                 cart.plusTocart(maSach, new Item(sach, soLuongMua), soLuongMua);
             }
             return Redirect("~/Home/index");
+        }
+        public ActionResult thongtingiohang()
+        {
+            return View();
+        }
 	}
 }
